13fa45f67bd5f06abf4a5ad3bd8228896e0984c4
Serkan Sevilgen <ssevilgen@gmail.com>
2024-10-27 14:01:53 +0300

    initial commit

---
+++ gendy.csd
@@ -0,0 +1,144 @@
+<CsoundSynthesizer>
+<CsOptions>
+; Select audio/midi flags here according to platform
+-odac     ;;;realtime audio out
+;-iadc    ;;;uncomment -iadc if real audio input is needed too
+; For Non-realtime ouput leave only the line below:
+; -o poscil3.wav -W ;;; for file output any platform
+</CsOptions>
+<CsInstruments>
+
+sr = 44100
+ksmps = 32
+nchnls = 2
+0dbfs  = 1
+
+gaReverb init 0
+
+instr Reverb
+  kRoomSize init 0.5
+  kHFDamp init 0.35 
+  aL, aR  freeverb gaReverb, gaReverb, kRoomSize, kHFDamp, sr, 0
+  outs aL, aR
+  clear gaReverb
+endin  
+
+schedule "Reverb", 0, -1
+
+instr 1 ;; Xenakis gendy
+  ;; kamp init 0.0 ;; 0.1 - 1.0
+  ;; kampdist init 1 ;; 1 - 6
+  ;; kdurdist init 0.001 ;; 0.0001 - 1
+  ;; kadpar init 0.0001 ;;  0.0001 - 1
+  ;; kddpar init 0.0001 ;;  0.0001 - 1
+  ;; kminfreq init 20 ;; 20 - 20000 
+  ;; kmaxfreq init 20 ;; 20 - 20000 
+  ;; kampscl init 0.1 ;; 0.1 - 1
+  ;; kdurscl init 0.1 ;; 0.1 - 1
+  ;; kgate init 0 ;; 0 / 1
+  ;; kpanpos init 0.0 ;; 0.0 - 1.0
+  ;; kRvbSendAmt init 0.0 ;; 0.0 - 1.0
+
+
+  kampdist random 1, 6
+  kamp init 0.3
+  kdurdist random 0.0001, 1
+  kadpar random 0.0001, 1
+  kddpar random 0.0001, 1
+  kminfreq random 20, 2000
+  kfreqdiff random 10, 100     
+  kmaxfreq = kminfreq + kfreqdiff
+  kampscl random 0.1, 1
+  kdurscl random 0.1, 1      
+  kgate init 1
+  kpanpos init 0.5
+  kRvbSendAmt init 0.2
+
+  kamp_channel chnget "kamp"
+  if kamp_channel != 0 then
+    kamp = kamp_channel
+  endif
+  kampdist_channel chnget "gendy_kampdist"
+  if kampdist_channel != 0 then
+    kampdist = kampdist_channel
+  endif
+  kdurdist_channel chnget "gendy_kdurdist"
+  if kdurdist_channel != 0 then
+    kdurdist = kdurdist_channel
+  endif
+  kadpar_channel chnget "gendy_kadpar"
+  if kadpar_channel != 0 then
+    kadpar = kadpar_channel
+  endif
+  kddpar_channel chnget "gendy_kddpar"
+  if kddpar_channel != 0 then
+    kddpar = kddpar_channel
+  endif
+  kminfreq_channel chnget "gendy_kminfreq"
+  if kminfreq_channel != 0 then
+    kminfreq = kminfreq_channel
+  endif
+  kmaxfreq_channel chnget "gendy_kmaxfreq"
+  if kmaxfreq_channel != 0 then
+    kmaxfreq = kmaxfreq_channel
+  endif
+  kampscl_channel chnget "gendy_kampscl"
+  if kampscl_channel != 0 then
+    kampscl = kampscl_channel
+  endif
+  kdurscl_channel chnget "gendy_kdurscl"
+  if kdurscl_channel != 0 then
+    kdurscl = kdurscl_channel
+  endif
+  kgate_channel chnget "kgate"
+  if kgate_channel != 0 then
+    kgate = kgate_channel
+  endif
+  kpanpos_channel chnget "kpanpos"
+  if kpanpos_channel != 0 then
+    kpanpos = kpanpos_channel
+  endif
+  kRvbSendAmt_channel chnget "kRvbSendAmt"
+  if kRvbSendAmt_channel != 0 then
+    kRvbSendAmt = kRvbSendAmt_channel
+  endif
+  
+  asig gendy kamp, kampdist, kdurdist, kadpar, kddpar, kminfreq, kmaxfreq,kampscl, kdurscl
+  kenv_prevent_click linsegr 0, 0.01, 1, p3-0.02, 1, 0.01, 0
+  asig = asig * kgate * kenv_prevent_click
+  aL, aR pan2 asig, kpanpos
+  outs aL, aR
+  gaReverb = gaReverb + (asig * kRvbSendAmt)          
+endin
+
+instr 10
+  knotedur init 1
+  krestdur init 1
+
+  knotedur_channel chnget "knotedur"
+  if knotedur_channel != 0 then
+    knotedur = knotedur_channel
+  endif
+  krestdur_channel chnget "krestdur"
+  if krestdur_channel != 0 then
+    krestdur = krestdur_channel
+  endif
+
+  loop:
+    kloopdur = knotedur + krestdur
+    iloopdur = i(kloopdur)
+    inotedur = i(knotedur)
+    timout    0, iloopdur, play
+      reinit    loop
+  play:
+    event_i "i", 1, 0, inotedur
+endin
+
+schedule 10, 0, -1
+
+
+</CsInstruments>
+<CsScore>
+
+</CsScore>
+</CsoundSynthesizer>
---
+++ main.js
@@ -0,0 +1,104 @@
+import { Csound } from '@csound/browser';
+import './style.css'
+import csd from './gendy.csd?raw'
+
+// oninput function
+window.sendData = async function (channel, value) {
+  // set channel
+  if(csound) await csound.setControlChannel(channel, value);
+  // update display
+  document.getElementById(channel+"val").innerHTML = value;
+}
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <button id='startButton'>Start Sound Engine</button>
+  </div>
+<hr />
+<div id="sliders">
+<p>
+<label for="kgate">On / Off (mute)</label> 
+<input type="checkbox" id="kgate"  oninput="sendData(id, value)">
+	      <span id="kgateval"> 0 </span> </input> 
+<p>
+<hr />
+<p>
+<label for="kamp">amp</label>  
+<input type="range" id="kamp" min="0" max="1" value="0" step="0.01"
+  oninput="sendData(id, value)"> <span id="kampval"> 0 </span> </input> 
+  </p>
+<p>
+<label for="kpanpos">Pan Position</label>  
+<input type="range" id="kpanpos" min="0" max="1" value="0.5" step="0.01"
+  oninput="sendData(id, value)"> <span id="kpanposval"> 0.5 </span> </input> 
+</p>
+<p>
+<label for="kRvbSendAmt">Reverb wet amount</label>  
+<input type="range" id="kRvbSendAmt" min="0" max="1" value="0" step="0.01"
+  oninput="sendData(id, value)"> <span id="kRvbSendAmtval"> 0 </span> </input> 
+</p>
+<hr />
+<p>
+<label for="knotedur">Note Duration</label>  
+<input type="range" id="knotedur" min="0.01" max="10" value="1" step="0.01"
+  oninput="sendData(id, value)"> <span id="knotedurval"> 1 </span> </input> 
+</p>
+<p>
+<label for="krestdur">Rest Duration</label>  
+<input type="range" id="krestdur" min="0" max="10" value="1" step="0.01"
+  oninput="sendData(id, value)"> <span id="krestdurval"> 1 </span> </input> 
+</p>
+<hr />
+<p>
+<label for="gendy_kampdist">gendy_kampdist</label>  
+<input type="range" id="gendy_kampdist" min="1" max="6" value="1" step="1"
+  oninput="sendData(id, value)"> <span id="gendy_kampdistval"> 1 </span> </input> 
+</p>    
+<label for="gendy_kdurdist">gendy_kdurdist</label>  
+<input type="range" id="gendy_kdurdist" min="0.001" max="1" value="0.001" step="0.001"
+  oninput="sendData(id, value)"> <span id="gendy_kdurdistval"> 0 </span> </input> 
+</p>    
+<label for="gendy_kadpar">gendy_kadpar</label>  
+<input type="range" id="gendy_kadpar" min="0.001" max="1" value="0.001" step="0.001"
+  oninput="sendData(id, value)"> <span id="gendy_kadparval"> 0 </span> </input> 
+</p>
+<label for="gendy_kddpar">gendy_kddpar</label>  
+<input type="range" id="gendy_kddpar" min="0.001" max="1" value="0.001" step="0.001"
+  oninput="sendData(id, value)"> <span id="gendy_kddparval"> 0 </span> </input> 
+</p>    
+<label for="gendy_kminfreq">gendy_kminfreq</label>  
+<input type="range" id="gendy_kminfreq" min="20" max="20000" value="20" step="1"
+  oninput="sendData(id, value)"> <span id="gendy_kminfreqval"> 20 </span> </input> 
+</p>    
+<label for="gendy_kmaxfreq">gendy_kmaxfreq</label>  
+<input type="range" id="gendy_kmaxfreq" min="21" max="20000" value="21" step="p11"
+  oninput="sendData(id, value)"> <span id="gendy_kmaxfreqval"> 21 </span> </input> 
+</p>    
+<label for="gendy_kampscl">gendy_kampscl</label>  
+<input type="range" id="gendy_kampscl" min="0.1" max="1" value="0.1" step="0.01"
+  oninput="sendData(id, value)"> <span id="gendy_kampsclval"> 0 </span> </input> 
+</p>    
+<label for="gendy_kdurscl">gendy_kdurscl</label>  
+<input type="range" id="gendy_kdurscl" min="0.1" max="1" value="0.1" step="0.01"
+  oninput="sendData(id, value)"> <span id="gendy_kdursclval"> 0.1 </span> </input> 
+</p>    
+</div>
+`
+
+let csound = null;
+
+const startCsound = async () => {
+  if(csound) {
+    return;
+  }
+
+  console.log("Starting Csound...");
+
+  csound = await Csound();
+  console.log(csound);
+  await csound.compileCsdText(csd);
+  await csound.start();
+}
+
+document.querySelector('#startButton').addEventListener('click', startCsound);
+
